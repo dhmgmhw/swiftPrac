c68d9ad6d110e4c3a324c600f5edddf045a13205
dhmgmhw <73983229+dhmgmhw@users.noreply.github.com>
2021-07-12 14:47:52 +0900

    Add files via upload

---
+++ Table/Table/AddViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddViewController.swift
+//  Table
+//
+//  Created by Moon on 2021/07/09.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBOutlet var tfAddItem: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func btnAddItem(_ sender: UIButton) {
+        items.append(tfAddItem.text!)
+        itemsImageFile.append("clock.png")
+        tfAddItem.text = ""
+        _ = navigationController?.popViewController(animated: true)
+    }
+}
